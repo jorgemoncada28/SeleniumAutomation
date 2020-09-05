e977d1963fb0b0f10ff383073c42e5cef04e745d
jjmoncada028 <jjmoncada029>
2020-09-05 00:45:34 -0500

    Sesion de operadores y tipos de datos

---
+++ SeleniumAutomation/src/test/java/com/javabasics/Datatypes.java
@@ -0,0 +1,52 @@
+package com.javabasics;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Datos numéricos
+		 */
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		
+//		x=127;
+//		y=32767;
+//		z = 2147483647;
+//		
+//		System.out.println(x);
+		
+		/*
+		 * Datos numéricos reales
+		 */
+//		float base;
+//		double altura;
+//		
+//		base = 30.5F;
+//		altura = 40.76;
+//		System.out.println(base);
+		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1= 'a';
+//		letra2= '\u0047';
+//		frase ="Hola";
+//		
+//		System.out.println(frase);
+
+//	boolean avesvuelan;
+//	
+//	avesvuelan=true;
+//	
+//	System.out.println("Las aves vuelan " + avesvuelan);
+		
+	
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasics/OperatorsStatements.java
@@ -0,0 +1,54 @@
+package com.javabasics;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Operadores Aritméticos
+		 */
+		
+//		int x=1;
+//		int y=2;
+//		int suma;
+//		
+//		
+//		suma=x+y;
+//		
+//		System.out.println(suma);
+		
+		/*
+		 * Operadores Unarios
+		 */
+//		int valor=3;
+//		
+//		int incrementar = --valor;
+//		
+//		System.out.println(incrementar);
+		
+		
+		/*
+		 * Operadores Relacionales
+		 */
+		
+		int num1=3;
+		int num2=3;
+		
+		if (num1 == num2) {
+			
+			System.out.println("Los números son iguales");
+		}else{
+			System.out.println("Los números no son iguales");
+			
+		}
+			
+			
+				
+		
+		
+		
+		
+	}
+
+}
